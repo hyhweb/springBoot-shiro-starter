2ff6dd0d60979c1e9becf7eeb867301a2bbad288
hyhweb <461342854@qq.com>
2019-10-27 17:05:28 +0800

    code

---
+++ src/main/java/com/springboot/shirostarter/ShirostarterApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.shirostarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShirostarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShirostarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/shirostarter/ShirostarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.shirostarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShirostarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
